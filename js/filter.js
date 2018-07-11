919db3a485a91cc4bed1d78d5f73c1f42ba44d92
kuronya <kirakiranekonya@gmail.com>
2018-07-11 17:48:57 +0900

    日記のページを別ファイルにした動くかな？

--- js/filter.js
+++ js/filter.js
@@ -1,19 +1,57 @@
-$(".nicobtn").click(function(){
-	$(".nikki").hide();
-	$(".nico").show();
+$(document).ready(function () {
+  $("#nikkilist").load("life/0.html");
 });
 
-$(".nozomibtn").click(function(){
-	$(".nikki").hide();
-	$(".nozomi").show();
+$(".nicobtn").click(function () {
+  $(".nikki").hide();
+  $(".nico").show();
 });
 
-
-$(".numazubtn").click(function(){
-	$(".nikki").hide();
-	$(".numazu").show();
+$(".elibtn").click(function () {
+  $(".nikki").hide();
+  $(".eli").show();
 });
 
-$(".allbtn").click(function(){
-	$(".nikki").show();
+$(".nozomibtn").click(function () {
+  $(".nikki").hide();
+  $(".nozomi").show();
+});
+
+$(".honokabtn").click(function () {
+  $(".nikki").hide();
+  $(".honoka").show();
+});
+
+$(".kotoribtn").click(function () {
+  $(".nikki").hide();
+  $(".kotori").show();
+});
+
+$(".umibtn").click(function () {
+  $(".nikki").hide();
+  $(".umi").show();
+});
+
+$(".rinbtn").click(function () {
+  $(".nikki").hide();
+  $(".rin").show();
+});
+
+$(".makibtn").click(function () {
+  $(".nikki").hide();
+  $(".maki").show();
+});
+
+$(".hanayobtn").click(function () {
+  $(".nikki").hide();
+  $(".hanayo").show();
+});
+
+$(".numazubtn").click(function () {
+  $(".nikki").hide();
+  $(".numazu").show();
+});
+
+$(".allbtn").click(function () {
+  $(".nikki").show();
 });
