3d611895c8f77b8b34c53461230f252e92a8f11e
kuronya <kirakiranekonya@gmail.com>
2018-07-11 10:37:43 +0900

    スクールアイドル日記にフィルタリング機能を付けた。

---
+++ js/filter.js
@@ -0,0 +1,19 @@
+$(".nicobtn").click(function(){
+	$(".nikki").hide();
+	$(".nico").show();
+});
+
+$(".nozomibtn").click(function(){
+	$(".nikki").hide();
+	$(".nozomi").show();
+});
+
+
+$(".numazubtn").click(function(){
+	$(".nikki").hide();
+	$(".numazu").show();
+});
+
+$(".allbtn").click(function(){
+	$(".nikki").show();
+});
